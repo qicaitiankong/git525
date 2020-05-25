9f2a9e393947b6b0a4ab695e1f95573d91e253c9
Lizihao Li <lizihaoli@Lizihaos-MacBook-Air.local>
2020-05-25 13:59:48 +0800

    去掉scendelagate

--- CocoapoadsTest/AppDelegate.m
+++ CocoapoadsTest/AppDelegate.m
@@ -17,25 +17,37 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *mainVC = [[UIViewController alloc]init];
+    mainVC.view.backgroundColor = [UIColor redColor];
+    self.window.rootViewController = mainVC;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    
 }
 
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    
 }
 
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    
+}
+
+-(void)applicationDidBecomeActive:(UIApplication *)application
+{
+    
+}
+
+-(void)applicationDidEnterBackground:(UIApplication *)application
+{
+    
+}
 
 @end
